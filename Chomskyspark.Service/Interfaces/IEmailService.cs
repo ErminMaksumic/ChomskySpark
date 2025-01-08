ab977e306950838737c83d6793de640851f44a5f
ErminMaksumic <ermin.maksumic@kodecta.com>
2025-01-08 22:32:06 +0100

    [T-33] Improvements

---
+++ Chomskyspark.Service/Interfaces/IEmailService.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Chomskyspark.Services.Interfaces
+{
+    public interface IEmailService
+    {
+         Task SendEmail(IConfiguration configuration, string receiverName, string receiverEmail, string subject, string message);
+    }
+}
